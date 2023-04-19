198fc52fa885cf95c1919ae68fae09b863a386d4
Popov Evgeny <eugene.eva.20.@gmail.com>
2023-04-19 14:41:05 +0600

    Created form add styled. Added lid EmailJS.

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,83 +1,111 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import emailjs from '@emailjs/browser'
 import styled from "styled-components";
 import Map from "./Map";
 
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 10px;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`;
+
+const LeftSection = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Title = styled.h3`
+  font-size: 24px;
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #eeeeee;
+  border: none;
+  border-radius: 5px;
+
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  background-color: #eeeeee;
+  border: none;
+  border-radius: 5px;
+
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: #fff;
+  font-weight: 700;
+  border: none;
+  border-radius: 5px;
+  padding: 15px;
+`;
+
+const Text = styled.p`
+  font-size: 16px;
+  font-weight: 200;
+  text-align: center;
+`;
+
+const RightSection = styled.div`
+  flex: 1;
+`;
+
 const Contacts = () => {
 
-    const Section = styled.div`
-      height: 100vh;
-      scroll-snap-align: center;
-      display: flex;
-      justify-content: center;
-    `;
+    const [success, setSuccess] = useState<boolean | null>(null);
+    const form = useRef<HTMLFormElement | any>(null);
 
-    const Container = styled.div`
-      width: 100%;
-      height: 100%;
-      padding: 0 10px;
-      display: flex;
-      justify-content: space-between;
-      gap: 50px;
-    `;
+    const handelSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
 
-    const LeftSection = styled.div`
-      flex: 1;
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-    `;
+        emailjs.sendForm('service_me9g7t1', 'template_w5wnd14', form.current, 'ja1NNE33VLXzFnp-_')
+            .then((result) => {
+                setSuccess(true);
+            }, (error) => {
+                setSuccess(false);
+            });
 
-    const Title = styled.h3`
-      font-size: 24px;
-      font-weight: 200;
-    `;
+        form.current.reset();
 
-    const Form = styled.form`
-      width: 500px;
-      display: flex;
-      flex-direction: column;
-      gap: 20px;
-    `;
-
-    const Input = styled.input`
-      padding: 20px;
-      background-color: #eeeeee;
-      border: none;
-      border-radius: 5px;
-      
-    `;
-
-    const TextArea = styled.textarea`
-      padding: 20px;
-      background-color: #eeeeee;
-      border: none;
-      border-radius: 5px;
-        
-    `;
-
-    const Button = styled.button`
-      background-color: #da4ea2;
-      color: #fff;
-      font-weight: 700;
-      border: none;
-      border-radius: 5px;
-      padding: 15px;
-    `;
-
-    const RightSection = styled.div`
-      flex: 1;
-    `;
+        setTimeout(() => {
+            setSuccess(null);
+        }, 3000);
+    }
 
     return (
         <Section>
             <Container>
                 <LeftSection>
-                    <Form>
+                    <Form ref={form} onSubmit={handelSubmit}>
                         <Title>Contact us</Title>
-                        <Input placeholder="Name"/>
-                        <Input placeholder="Emial"/>
-                        <TextArea placeholder="Write your message" rows={10}/>
-                        <Button>Send</Button>
+                        <Input placeholder="Name" name="name" type="text" required/>
+                        <Input placeholder="Email" name="email" type="email" required/>
+                        <TextArea placeholder="Write your message" name="message" required rows={10}/>
+                        <Button type="submit">Send</Button>
+                        {success && (<Text>Your message has been sent!</Text>)}
                     </Form>
                 </LeftSection>
                 <RightSection>
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,6 +1,73 @@
 import React, {useRef} from 'react';
 import styled from "styled-components";
 
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 1300px;
+  padding: 0 10px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const LeftSection = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ListItem = styled.li<any>`
+  font-size: 75px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px #fff;
+  position: relative;
+
+  ::after {
+    content: "${(props) => {
+      return props.text;
+    }}";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    overflow: hidden;
+    color: #ffffff;
+    white-space: nowrap;
+  }
+
+  &:hover {
+    ::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to {
+          width: 100%;
+        }
+      }
+
+    }
+  }
+`;
+
+const RightSection = styled.div`
+  flex: 1;
+`;
+
 const Works = () => {
     const list = useRef<string[]>([
         'Web Design',
@@ -9,72 +76,6 @@ const Works = () => {
         'Product Design',
         'Social Media'
     ]);
-
-    const Section = styled.div`
-      height: 100vh;
-      scroll-snap-align: center;
-      display: flex;
-      justify-content: center;
-    `;
-
-    const Container = styled.div`
-      width: 1300px;
-      padding: 0 10px;
-      display: flex;
-      justify-content: space-between;
-    `;
-
-    const LeftSection = styled.div`
-      flex: 1;
-      display: flex;
-      align-items: center;
-    `;
-
-    const List = styled.ul`
-      list-style: none;
-      display: flex;
-      flex-direction: column;
-      gap: 20px;
-    `;
-
-    const ListItem = styled.li<any>`
-      font-size: 75px;
-      font-weight: bold;
-      cursor: pointer;
-      color: transparent;
-      -webkit-text-stroke: 1px #fff;
-      position: relative;
-
-      ::after {
-        content: "${(props) => {
-          return props.text;
-        }}";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 0;
-        overflow: hidden;
-        color: #ffffff;
-        white-space: nowrap;
-      }
-
-      &:hover {
-        ::after {
-          animation: moveText 0.5s linear both;
-
-          @keyframes moveText {
-            to {
-              width: 100%;
-            }
-          }
-
-        }
-      }
-    `;
-
-    const RightSection = styled.div`
-      flex: 1;
-    `;
 
     return (
         <Section>
